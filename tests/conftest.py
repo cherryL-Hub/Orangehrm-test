3db081f7da6551a6fd8b35c78d81c58ab44097cb
19211 <1921115270@qq.com>
2026-06-21 14:21:56 +0800

    修正4版

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,7 +27,39 @@ def api_client_raw():
 
 
 @pytest.fixture()
+def api_client(api_client_raw):
+    base_url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/validate"
+    session, token = api_client_raw
+    login_data = {
+        "_token": token,
+        "username": "Admin",
+        "password": "admin123"
+    }
+
+    response = session.post(base_url, data=login_data)
+
+    assert response.status_code == 200
+
+    return Apiclient(session)
+
+
+@pytest.fixture()
 def driver():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     yield driver
     driver.quit()
+
+
+@pytest.fixture()
+def login_driver(driver):
+    driver.get("https://opensource-demo.orangehrmlive.com/")
+    time.sleep(10)
+
+    username = "Admin"
+    password = "admin123"
+
+    driver.find_element(By.NAME, "username").send_keys(username)
+    driver.find_element(By.NAME, "password").send_keys(password)
+    driver.find_element(By.XPATH, "//button[@type='submit']").click()
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "oxd-userdropdown-tab")))
+    return driver
